dcb1fea62d98dc237f5e0b9b87790b0a71855cbb
Ethan Simental <esimental@drury.edu>
2025-03-17 16:48:38 -0500

    EthanSimental

--- edit_me.py
+++ edit_me.py
@@ -1,14 +1,16 @@
-#Ethan Simental
+#finished quality content
 def find_max(lst):
-    """return the maximum element"""
+
 
     current_max = lst[0]
+    #lists the max
 
     for i in range(1, len(lst)):
         if lst[i] > current_max:
             current_max = lst[i]
 
     return current_max
+    #returns the max
 
 test_list = [2112*i % 2024 for i in range(10000)]
 
