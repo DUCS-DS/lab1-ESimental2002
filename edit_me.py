b560102a13111aab245d7a18a647162b16dcfdea
ESimental2002 <esimental@drury.edu>
2025-04-28 16:16:08 -0500

    commit
    commit

--- edit_me.py
+++ edit_me.py
@@ -3,14 +3,12 @@ def find_max(lst):
 
 
     current_max = lst[0]
-    #lists the max
 
     for i in range(1, len(lst)):
         if lst[i] > current_max:
             current_max = lst[i]
 
     return current_max
-    #returns the max
 
 test_list = [2112*i % 2024 for i in range(10000)]
 
